5e6553d90e44b44db2dae56608e8b90518135e4d
reg000 <chimchartorcoal@gmail.com>
2023-02-26 22:12:41 +0530

    selection list dropdown, displaying respected char

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const letter = document.querySelector(".letter");
+const charf = document.getElementById("char-f")
+const chars = document.getElementById("char-s")
+const chart = document.getElementById("char-t")
+
+function dropdown(char) {
+    const character = document.getElementById("Character");
+    const charObj = document.getElementById("char-" + char);
+    
+    character.classList.add("clicked");
+    
+    setTimeout(function(){
+        character.style.display = "none";
+        charObj.style.display = "inline-block";
+        character.classList.remove("clicked");
+    }, 1000);
+}
